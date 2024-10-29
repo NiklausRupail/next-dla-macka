e447e4a2d58b40ebf8c5ebd009f25f29e052ae6f
maciekw5 <wojciechowski.dev@gmail.com>
2024-10-29 10:57:18 +0100

    footer

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    
+    return (
+        <footer className="flex justify-center py-5">
+            <p>To jest testowy footer</p>
+        </footer>
+    )
+
+}
+
+export default Footer;
